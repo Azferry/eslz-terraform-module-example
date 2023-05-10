ece82df4c08e5594ba370cc5f32aa76f6a328a08
Ryan Ferry <ryanferry@microsoft.com>
2023-05-10 16:27:54 -0400

    VWAN Enable

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG"
-    storage_account_name = "SA"
-    container_name       = "Container"
+    resource_group_name  = "ntc-mgt-terra-wus3-p-rg01"
+    storage_account_name = "ntcmgtterrawus3psa01"
+    container_name       = "tfstate-eslzdemo"
     key                  = "eslz-demo.tfstate"
   }
 }
